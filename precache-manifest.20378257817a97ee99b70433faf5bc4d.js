11f2a033f74141ff4e6acbb72e913dd43afd8260
Akashamba <akashambashankar@gmail.com>
2020-10-18 16:59:44 +0530

    Updates

---
+++ precache-manifest.20378257817a97ee99b70433faf5bc4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7a67529017a6ffd343cf160e676ecd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c1257af2c4fd549e63a3",
+    "url": "/monsters-rolodex/static/css/main.a182d2ef.chunk.css"
+  },
+  {
+    "revision": "2d19631dba197ad7891e",
+    "url": "/monsters-rolodex/static/js/2.e1627e68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.e1627e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1257af2c4fd549e63a3",
+    "url": "/monsters-rolodex/static/js/main.6930f2f5.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
